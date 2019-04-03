2177675ef02e3a2857d32fb632cd2adf444ce08a
Edivaldo <you@example.com>
2019-04-02 23:01:14 -0300

    remover arquivos

--- src/main/java/com/edivaldo/controller/Controller.java
+++ src/main/java/com/edivaldo/controller/Controller.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,12 +16,13 @@ import com.edivaldo.model.UserEntity;
 import com.edivaldo.service.TweetsServicesImp;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/api")
 public class Controller {
 
 	@Autowired
 	private TweetsServicesImp TweetsServicesImp;
-
+	
+	@CrossOrigin
 	@GetMapping("/userComMaiSeguidores")
 	public ResponseEntity<ResponseDefault> userComMaiSeguidores() {
 		List<UserEntity> usersBySeguidoresLimit5 = TweetsServicesImp.getUsersBySeguidoresLimit5();
@@ -31,6 +33,7 @@ public class Controller {
 		return idiomaResponse != null ? ResponseEntity.ok(idiomaResponse) : ResponseEntity.notFound().build();
 	}
 
+	@CrossOrigin
 	@GetMapping("/postagensPorHora")
 	public ResponseEntity<ResponseDefault> postagensPorHora() {
 		List<Postagem> listaPostagens = TweetsServicesImp.getTweets();
@@ -39,7 +42,20 @@ public class Controller {
 		idiomaResponse.setObj(listaPostagens);
 		return idiomaResponse != null ? ResponseEntity.ok(idiomaResponse) : ResponseEntity.notFound().build();
 	}
+	
+	
+	@CrossOrigin
+	@GetMapping("/hashTagPorIdioma")
+	public ResponseEntity<List<HashTagOdt>> hashTagPorIdioma() {
+		List<HashTagOdt> hashTagPorIdimoPais = TweetsServicesImp.hashTagPorIdimoPais();
 
+		ResponseDefault idiomaResponse = new ResponseDefault();
+		idiomaResponse.setApiVersion("1.0");
+		idiomaResponse.setObj(hashTagPorIdimoPais);
+		return idiomaResponse != null ? ResponseEntity.ok(hashTagPorIdimoPais) : ResponseEntity.notFound().build();
+	}
+	
+/*	@CrossOrigin
 	@GetMapping("/hashTagPorIdioma")
 	public ResponseEntity<ResponseDefault> hashTagPorIdioma() {
 		List<HashTagOdt> hashTagPorIdimoPais = TweetsServicesImp.hashTagPorIdimoPais();
@@ -48,6 +64,6 @@ public class Controller {
 		idiomaResponse.setApiVersion("1.0");
 		idiomaResponse.setObj(hashTagPorIdimoPais);
 		return idiomaResponse != null ? ResponseEntity.ok(idiomaResponse) : ResponseEntity.notFound().build();
-	}
+	}*/
 
 }
